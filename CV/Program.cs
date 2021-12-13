689e9b08049bb8e4ff16f9a245c7f52a14ef5e15
Alex Muhortov <44553240+MuhortovAA@users.noreply.github.com>
2021-12-13 16:24:10 +0300

    add

---
+++ CV/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covariant4App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bears = new Stack<Bear>();
+            var camel = new Stack<Camel>();
+            IMoveable<Animal> animal = new Stack<Animal>();
+
+            bears.Push();
+            bears.Pop();
+            IPoppable<Animal> animals1 = bears;
+            IPushable<Animal> animals2 = camel;
+            //IMoveable<Bear> bears1 = animal;
+            IMoveable<Bear> bears1 = animal;
+            animals1.Pop();
+            animals2.Push();
+            bears1.Move(new Bear());
+        }
+
+
+    }
+    public interface IPoppable<out T> { T Pop(); }
+    public interface IMoveable<in T> { void Move(T obj); }
+    public interface IPushable<out T> { void Push(); }
+    class Animal { }
+
+    class Bear : Animal { }
+
+    class Camel : Animal { }
+    /// <summary>
+    /// 1
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T> : IPoppable<T>, IPushable<T>, IMoveable<T>
+    {
+        int position;
+        T[] data = new T[100];
+
+        public void Move(T obj)
+        {
+            throw new NotImplementedException();
+        }
+        public T Pop() => data[--position];
+
+        public void Push()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    /// <summary>
+    /// 2
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    //public class Stack<T>
+    //{
+    //    int position;
+    //    T[] data = new T[100];
+    //    public void Push(T obj) => data[position++] = obj;
+    //    public T Pop() => data[--position];
+    //}
+
+}
